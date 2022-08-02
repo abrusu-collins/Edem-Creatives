e0fb4d7dfc294817bed526a4a6b015d42244fe61
abrusu-collins <snillochemingson@gmail.com>
2022-08-02 11:36:27 +0000

    Done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var navbar = document.querySelector('.nav')
 window.onscroll = function() {
 
   // pageYOffset or scrollY
-  if (window.pageYOffset > 600 ) {
+  if (window.pageYOffset > 0 ) {
     navbar.classList.add('scrolled')
   } else {
     navbar.classList.remove('scrolled')
